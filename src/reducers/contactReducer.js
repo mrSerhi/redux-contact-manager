5ee7dc9fc52a961169f3aba44e27175b18d109f6
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-03-02 21:37:57 +0200

    set up redux and redux dev-tools

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+const contactReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default contactReducer;
