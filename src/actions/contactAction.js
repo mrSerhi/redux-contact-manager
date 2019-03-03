eff965bc568bcf25a679db8e47281886ed0a01f7
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-03-03 13:42:08 +0200

    was added the actions on delete and add contact. Change main reducer

--- src/actions/contactAction.js
+++ src/actions/contactAction.js
@@ -1,3 +1,7 @@
-import { GET_CONTACTS } from "./types";
+import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from "./types";
 
 export const getContacts = () => ({ type: GET_CONTACTS }); // returns dispatch fn with action type
+
+export const deleteContact = id => ({ type: DELETE_CONTACT, payload: id });
+
+export const addContact = contact => ({ type: ADD_CONTACT, payload: contact });
